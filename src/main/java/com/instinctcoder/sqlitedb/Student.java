97ed2377a0543e66ce6672b58f34d3f30787bab4
Pankaj <pank.kmwt@gmail.com>
2016-01-24 13:36:03 +0530

    Adding project

---
+++ src/main/java/com/instinctcoder/sqlitedb/Student.java
@@ -0,0 +1,52 @@
+package com.instinctcoder.sqlitedb;
+
+import android.content.Context;
+
+public class Student {
+    // Labels table name
+    public static final String TABLE = "Student";
+
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_age = "age";
+
+    public int student_ID;
+    public String name;
+    public String email;
+    public int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudent_ID() {
+        return student_ID;
+    }
+
+    public void setStudent_ID(int student_ID) {
+        this.student_ID = student_ID;
+    }
+
+}
